108f3eef897f13d61e8a3351491ff57fb30550f3
visualfc <visualfc@gmail.com>
2021-11-09 09:38:55 +0800

    update gomonkey test

--- internal/gomonkey/func_test.go
+++ internal/gomonkey/func_test.go
@@ -14,6 +14,22 @@ func TestMakeFunc(t *testing.T) {
 	})
 	v := fn.Interface().(func(int) string)(100)
 	if v != "str:100" {
-		t.Failed()
+		t.Fail()
+	}
+}
+
+type TestA struct {
+	N int
+}
+
+func TestMakeMethod(t *testing.T) {
+	typ := reflect.FuncOf([]reflect.Type{reflect.TypeOf((*TestA)(nil))}, []reflect.Type{reflect.TypeOf("")}, false)
+	fn := MakeFunc(func() {}, typ, func(args []reflect.Value) []reflect.Value {
+		r := fmt.Sprintf("str:%v", args[0].Elem().Field(0))
+		return []reflect.Value{reflect.ValueOf(r)}
+	})
+	v := fn.Interface().(func(*TestA) string)(&TestA{200})
+	if v != "str:200" {
+		t.Fail()
 	}
 }
